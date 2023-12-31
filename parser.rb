7a677eed0f8336250373936587639a7b5c806636
Finn Bacall <finn.bacall@manchester.ac.uk>
2023-12-31 19:42:02 +0000

    Parse regions and spawns

--- parser.rb
+++ parser.rb
@@ -29,29 +29,83 @@ class Parser
     mission_data = {}
     PLANETS.each do |planet|
       file = File.join(mission_base, "#{planet}_destroy_missions.lua")
-      puts file
       mission_data[planet] = parse_mission_lua(file)
     end
     mission_data.each do |planet, lairs|
       lairs.map! do |lair|
-        convert_mission(lair.merge(mobs: lair_data(lair[:lairTemplateName], planet)))
+        convert_mission(lair.merge(mobs: lair_data[lair[:lairTemplateName]][:mobs]))
       end
     end
     mission_data
   end
 
-  def lair_data(lair, planet)
-    lair = lair.sub('spinded', 'spined') # Typo fix
-    path = File.join(lair_base, 'creature_lair', planet, "#{lair}.lua")
-    path = File.join(lair_base, 'npc_theater', planet, "#{lair}.lua") unless File.exist?(path)
-    path = File.join(lair_base, 'creature_dynamic', planet, "#{lair}.lua") unless File.exist?(path)
-    path = File.join(lair_base, 'npc_dynamic', planet, "#{lair}.lua") unless File.exist?(path)
-    unless File.exist?(path)
-      STDERR.puts "Can't find path for lair: #{lair}, skipping!"
-      return []
+  def lair_data
+    return @lair_data if @lair_data
+    @lair_data = {}
+    (PLANETS + ['global']).each do |loc|
+      ['creature_lair', 'npc_theater', 'creature_dynamic', 'npc_dynamic'].each do |type|
+        files = Dir.glob(File.join(lair_base, type, loc, "*.lua"))
+        puts "#{loc} #{type} (#{files.size}): "
+        files.each do |file|
+          lair = parse_lair_lua(file)
+          raise "Lair name collision #{lair[:name]}" if @lair_data[lair[:name]]
+          @lair_data[lair[:name]] = lair
+          print '.'
+        end
+        puts
+      end
+    end
+    @lair_data
+  end
+
+  def spawn_data
+    return @spawn_data if @spawn_data
+    @spawn_data = {}
+    PLANETS.each do |planet|
+      files = Dir.glob(File.join(spawn_base, planet, '*.lua'))
+      puts "#{planet} (#{files.size}): "
+      files.each do |file|
+        spawn = convert_spawns(parse_spawn_lua(file))
+        warn "Spawn name collision #{spawn[:name]}" if @spawn_data[spawn[:name]]
+        @spawn_data[spawn[:name]] = spawn.merge(mobs: spawn[:lairs].map { |l| lair_data[l[:name]][:mobs].map(&:first) }.flatten )
+        print '.'
+      end
+      puts
+    end
+    @spawn_data
+  end
+
+  def region_data
+    region_data = {}
+    PLANETS.each do |planet|
+      file = File.join(region_base, "#{planet}_regions.lua")
+      region_data[planet] = {}
+      convert_regions(parse_region_lua(file)).each do |region|
+        raise "Region name collision #{region[:name]}" if region_data[planet][region[:name]]
+        region_data[planet][region[:name]] = region.merge(mobs: region[:spawns].map { |s| spawn_data[s][:mobs] }.flatten )
+      end
+      print '.'
     end
 
-    parse_lair_lua(path)
+    region_data
+  end
+
+  def static_spawn_data
+    # TODO: Still needs work, not everything in the static spawn file is appropriate to include
+    #       the Yavin4 static spawn file also loads mobs from an array.
+    static_spawn_data = {}
+    Dir.glob(File.join(static_spawn_base, '*.lua')).each do |file|
+      parse_static_spawn_lua(file).each do |static_spawn|
+        planet = static_spawn.delete(:planet)
+        static_spawn_data[planet] ||= {}
+        mob = static_spawn.delete(:mob)
+        static_spawn_data[planet][mob] ||= []
+        static_spawn_data[planet][mob] << [static_spawn[:x], static_spawn[:y]]
+      end
+      print '.'
+    end
+
+    static_spawn_data
   end
 
   def creature_base
@@ -64,6 +118,18 @@ class Parser
 
   def lair_base
     File.join(@base, 'MMOCoreORB', 'bin', 'scripts', 'mobile', 'lair')
+  end
+
+  def region_base
+    File.join(@base, 'MMOCoreORB', 'bin', 'scripts', 'managers', 'planet')
+  end
+
+  def spawn_base
+    File.join(@base, 'MMOCoreORB', 'bin', 'scripts', 'mobile', 'spawn')
+  end
+
+  def static_spawn_base
+    File.join(@base, 'MMOCoreORB', 'bin', 'scripts', 'screenplays', 'static_spawns')
   end
 
   private
@@ -156,6 +222,10 @@ class Parser
 
     begin
       d = eval(buffer)
+      # Parse creature name from start of file
+      match_data = data.match(/([a-zA-Z0-9_-]+)\s*=\s*Lair:new\s*{/)
+      raise "Couldn't match lair name" if match_data.nil?
+      name = match_data[1]
     rescue Exception => e
       STDERR.puts "!!! ERROR parsing #{file}"
       STDERR.puts data
@@ -164,7 +234,68 @@ class Parser
       raise e
     end
 
+    { name: name, mobs: d }
+  end
+
+  def parse_spawn_lua(file)
+    data = File.read(file)
+    data.gsub!(/\s*\-\-\[\[.+\]\]/m, "\n") # Block comments
+    data.gsub!(/\s*\-\-[^\n]*\n/,"\n") # Comments
+    data.gsub!(/,\s*\-\-[^\n]*\n/,",\n") # Comments following ,
+    data.gsub!('{', '[') # Array open
+    data.gsub!('}', ']') # Array close
+    data.gsub!(';', ',') # Some lines end in semi-colon instead of comma for some reason
+    data.gsub!(/\s*=\s*/, ': ') # = to :
+    data.sub!(/\s*lairSpawns\s*:\s*\[/, '[')
+    data.sub!(/\s*addSpawnGroup.*/, '')
+
+    begin
+      # Parse spawn name from start of file
+      match_data = data.match(/([a-zA-Z0-9_-]+)\s*:\s*\[/)
+      raise "Couldn't match spawn name" if match_data.nil?
+      name = match_data[1]
+      data.sub!(match_data[0], '[')
+      d = eval(data)
+    rescue Exception => e
+      STDERR.puts "!!! ERROR parsing #{file}"
+      STDERR.puts data
+      raise e
+    end
+
+    { name: name, lairs: d.flatten }
+  end
+
+  def parse_region_lua(file)
+    data = File.read(file)
+    data.gsub!(/\s*\-\-\[\[.+\]\]/m, "\n") # Block comments
+    data.gsub!(/\s*\-\-[^\n]*\n/,"\n") # Comments
+    data.gsub!(/,\s*\-\-[^\n]*\n/,",\n") # Comments following ,
+    data.gsub!('{', '[') # Array open
+    data.gsub!('}', ']') # Array close
+    data.gsub!(';', ',') # Some lines end in semi-colon instead of comma for some reason
+    data.gsub!(/\s*=\s*/, ': ') # = to :
+    data.sub!(/.+_regions\s*:\s*\[/, '[')
+    data.sub!(/\s*require.*/, '')
+
+    begin
+      d = eval(data)
+    rescue Exception => e
+      STDERR.puts "!!! ERROR parsing #{file}"
+      STDERR.puts data
+      raise e
+    end
+
     d
+  end
+
+  def parse_static_spawn_lua(file)
+    data = File.read(file)
+    data.scan(/spawnMobile\(.+/).map do |s|
+      s.sub!('spawnMobile(', '')
+      s.gsub!('"','')
+      s = s.split(',').map(&:strip)
+      { planet: s[0].gsub('"',''), mob: s[1], x: s[3].to_f, y: s[5].to_f }
+    end
   end
 
   def convert(output)
@@ -220,6 +351,42 @@ class Parser
     end
   end
 
+  def convert_spawns(spawn)
+    s = []
+    total = 0
+    spawn[:lairs].each do |lair|
+      begin
+        output = {}
+        output[:name] = lair[:lairTemplateName]
+        output[:min_cl] = lair[:minDifficulty]
+        output[:max_cl] = lair[:maxDifficulty]
+        output[:weighting] = lair[:weighting]
+        total += output[:weighting]
+        s << convert(output)
+      rescue Exception => e
+        STDERR.puts "!!! ERROR converting"
+        STDERR.puts lair.inspect
+        raise e
+      end
+    end
+    s.each { |spawn| spawn[:chance] = spawn[:weighting].to_f / total }
+    spawn[:lairs] = s
+    spawn
+  end
+
+  def convert_regions(regions)
+    regions.map do |region|
+      next unless region[4].to_ary.any? { |r| r.to_s == 'SPAWNAREA' }
+      r = {}
+      r[:name] = region[0]
+      x, y = region[1], region[2]
+      shape = region[3]
+      r[:spawns] = region[5]
+      r[:shape] = [shape.shift.to_s.downcase, x, y, *shape]
+      r
+    end.compact
+  end
+
   def parse_resist(resist)
     return 'Vul' if resist == -1
     return 0 if resist == 0
@@ -262,6 +429,10 @@ end
 parser = Parser.new(ARGV[0])
 mob_data = parser.mob_data
 mission_data = parser.mission_data
+#lair_data = parser.lair_data
+#spawn_data = parser.spawn_data
+region_data = parser.region_data
+static_spawn_data = parser.static_spawn_data
 
 TYPES = {
   'MOB_HERBIVORE' => 'creatures',
@@ -283,3 +454,7 @@ categorized.each do |category, mobs|
 end
 
 File.write(File.join('data', "missions.json"), JSON.pretty_generate(mission_data))
+#File.write(File.join('data', "lairs.json"), JSON.pretty_generate(lair_data))
+File.write(File.join('data', "regions.json"), JSON.pretty_generate(region_data))
+#File.write(File.join('data', "spawns.json"), JSON.pretty_generate(spawn_data))
+# File.write(File.join('data', "static_spawns.json"), JSON.pretty_generate(static_spawn_data))
